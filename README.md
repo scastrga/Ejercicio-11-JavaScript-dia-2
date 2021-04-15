980130566402f01eaafff2f100fa9c4e4b9fbb99
scastrga <scastrga@everis.com>
2021-04-15 21:43:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vgzrrlejercicio11-l9hy7a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vgzrrlejercicio11-l9hy7a)
---
+++ index.js
@@ -0,0 +1,7 @@
+var div = document.getElementById("container");
+
+div.parentNode.removeChild(div);
+
+var button = document.getElementById("button");
+
+button.parentNode.removeChild(button);
